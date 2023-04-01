2208941d59e0b1c9468f5442a5ee3d53113f1700
Mateus <mateusmba20@gmail.com>
2023-04-01 20:39:32 -0300

    first commit

---
+++ src/utils/formatCurrency.ts
@@ -0,0 +1,6 @@
+export default function formatCurrency(value: number) {
+  /** Converte o número para o formato BRL */
+  return new Intl.NumberFormat('pt-br',
+    { style: 'currency', currency: 'BRL'}
+  ).format(value);
+}
