e95a3519aab97003fe97defda99a8e8b5be60737
Mateus Araujo <mateusmba20@gmail.com>
2024-05-29 20:43:29 -0300

    feat: Create Router component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { GlobalStyles } from './styles/GlobalStyles';
-import { Header } from './components/Header';
-import { Orders } from './components/Orders';
+import { Router } from './Router';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Header />
-      <Orders />
+      <Router />
       <ToastContainer position='bottom-center' />
     </>
   );
