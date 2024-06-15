acffd0f653163a56049caee51010366cb558f2a8
Mateus Araujo <mateusmba20@gmail.com>
2024-06-15 16:19:47 -0300

    fix: renames styles.tsx to styles.ts

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Overlay = styled.div`
   left: 0px;
   top: 0px;
+  z-index: 1;
   background: rgba(0, 0, 0, 0.8);
   backdrop-filter: blur(4.5px);
   width: 100%;
