102818c458ea959f7263c25f0d44d3df445f7868
Mateus Araujo <mateusmba20@gmail.com>
2024-06-15 16:14:00 -0300

    fix: improves Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,13 @@ import { Container } from './styles';
 
 interface ButtonProps extends ComponentProps<'button'> {
   label: string;
+  variant?: 'primary' | 'secondary';
+  onClick?: () => void;
 }
 
-export function Button({ label, disabled }: ButtonProps) {
+export function Button({ label, disabled, variant, onClick }: ButtonProps) {
   return (
-    <Container disabled={disabled} className='primary '>
+    <Container disabled={disabled} variant={ variant || 'primary' } onClick={onClick}>
       <span>{ label }</span>
     </Container>
   );
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,16 +1,18 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.button`
+export const Container = styled.button<{ variant: string }>`
   width: 100%;
   background: #D73035;
   border: 0;
   border-radius: 48px;
   color: #fff;
   padding: 12px 24px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
+
+  ${({ variant }) => variant === 'secondary' ? css`
+    color: #D73035;
+    background: transparent;
+    padding: 12px 0px;
+  ` : ''}
 
   :disabled {
     background-color: #ccc;
