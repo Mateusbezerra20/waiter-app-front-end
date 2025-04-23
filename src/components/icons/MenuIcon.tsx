6d28d0a6752cb5314fde25c5c20259631467a131
Mateus Araujo <mateus@Mateus>
2025-04-22 21:53:10 -0300

    fix: menu and profile icons

--- src/components/icons/MenuIcon.tsx
+++ src/components/icons/MenuIcon.tsx
@@ -1,6 +1,13 @@
 export function MenuIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      viewBox="0 0 24 24"
+      height="24"
+      data-name="Layer 1"
+      fill="none"
+    >
       <path
         stroke="currentColor"
         strokeLinecap="round"
