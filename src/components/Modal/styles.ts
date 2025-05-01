b982d9c77b00800d24ec6ec2f8d1ebc1ab6f6849
Mateus Araujo <mateusmba20@gmail.com>
2025-04-30 21:40:13 -0300

    style: add fadeIn and scaleIn animations in Modal/styles.ts

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,10 +1,30 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const scaleIn = keyframes`
+  from {
+    scale: 0;
+  }
+
+  to {
+    scale: 1;
+  }
+`;
 
 export const Overlay = styled.div`
   left: 0px;
   top: 0px;
   z-index: 1;
-  background: rgba(0, 0, 0, 0.8);
+  background: rgba(0, 0, 0, 0.5);
   backdrop-filter: blur(4.5px);
   width: 100%;
   height: 100%;
@@ -12,12 +32,14 @@ export const Overlay = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  animation: ${fadeIn} 0.3s;
 `;
 
 export const ModalBody = styled.div`
   background: #fff;
   border-radius: 8px;
   padding: 2rem;
+  animation: ${scaleIn} 0.2s;
 
   header {
     display: flex;
